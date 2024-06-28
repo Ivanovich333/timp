ec77b04bf7a050c156e550176403d4141ce53b16
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-06-28 10:24:42 +0300

    123

---
+++ build/CMakeFiles/test_foo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_foo.dir/tests/test_foo.cpp.o"
+  "CMakeFiles/test_foo.dir/tests/test_foo.cpp.o.d"
+  "test_foo"
+  "test_foo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/timp
+# Build directory: /home/vboxuser/timp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_foo "/home/vboxuser/timp/build/test_foo")
+set_tests_properties(test_foo PROPERTIES  _BACKTRACE_TRIPLES "/home/vboxuser/timp/CMakeLists.txt;11;add_test;/home/vboxuser/timp/CMakeLists.txt;0;")
